5a4cb038857081862ff2e9bd71f36cf361f00050
Marcel <marcel.adam@mni.thm.de>
2026-02-10 13:49:55 +0100

    rework(howtoconnect): set realmlist entfernt

--- template/icecrown/tpl/howtoconnect.php
+++ template/icecrown/tpl/howtoconnect.php
@@ -11,6 +11,4 @@
     <p>2. <?php elang('create_account_tip2'); ?></p>
     <p>3. <?php elang('create_account_tip3'); ?></p>
     <p>4. <?php elang('create_account_tip4'); ?></p>
-    <p style="text-align: center;font-weight: bold;color:darkred">set
-        realmlist <?php echo get_config('realmlist'); ?></p>
 </div>
